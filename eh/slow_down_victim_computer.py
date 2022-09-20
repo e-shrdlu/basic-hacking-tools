72ef1feb2250e87456c34802caa408d23e02fbfc
Nat (Nathan) Broyles <50746727+e-shrdlu@users.noreply.github.com>
2022-09-19 19:43:22 -0500

    Delete eh directory

--- eh/slow_down_victim_computer.py
+++
@@ -1,8 +0,0 @@
-# aparently this will freeze victims screen?
-# okay thats kinda lame
-import keyboard
-
-while True:
-    if keyboard.is_pressed('q'):
-        break
-    keyboard.write('asdfghjkl;a;xiwk,z,jkxdiak,xk')
